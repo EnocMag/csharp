819a3c44c4dd025bdf912baf3ea6b8db23ece1b1
Enoc M <magallon.estrada.e@gmail.com>
2024-10-10 21:01:06 -0700

    Petición de datos al usuario e impresión de saludo

---
+++ PeticionDatos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Por favor ingresa tu nombre: ");
+         string nombre = Console.ReadLine() ?? "Desconocido";
+
+        Saludar(nombre);
+    }
+
+    static void Saludar(string nombre)
+    {
+        Console.WriteLine($"Hola, {nombre}! Bienvenido :)");
+    }
+}
