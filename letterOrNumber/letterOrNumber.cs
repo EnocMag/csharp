e9100015a8c3584a7c7ff6c0c1027113c5989753
Enoc M <magallon.estrada.e@gmail.com>
2025-03-03 12:49:42 -0700

    Determina si un carácter ingresado es letra o número

---
+++ letterOrNumber/letterOrNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+class LetterOrNumber
+{
+    static void Main(string[] args)
+    {
+        Console.Write($"Ingrese caracter: ");
+
+
+        if (char.TryParse(Console.ReadLine(), out char myChar))
+        {
+            if (char.IsLetter(myChar))
+            {
+                if (char.IsLower(myChar))
+                {
+                    Console.WriteLine($"{myChar} es una letra minúscula");
+                }
+                else
+                {
+                    Console.WriteLine($"{myChar} es una letra mayúscula");
+                }
+            }
+            else if (char.IsDigit(myChar))
+            {
+                Console.WriteLine($"{myChar} es un número.");
+            }
+            else
+            {
+                Console.WriteLine($"{myChar} es un símbolo.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Ingresa solo un caracter");
+        }
+    }
+}
